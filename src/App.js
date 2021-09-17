cbb41f02727c722555c7144f32091481af10b8a5
dynamic-henry-kwon <jukdo67@gmail.com>
2021-09-17 17:40:55 +0900

    css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Component from './components/Component.js';
 import { api } from './api/api.js';
 export default class App extends Component {
   constructor(target) {
-    super(target, 'div', { innerHTML: 'test' });
+    super(target, 'div', { innerHTML: '' });
     this.bindEvents();
   }
 
   onClick = () => {
     this.tryFetchData(api.getRandomCats, 'test', {
       cb: (data) => {
+        console.log(data);
         return data;
       },
     });

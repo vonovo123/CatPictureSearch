c6114a5b1abb58e26a844c99a77bb655548e3c17
dynamic-henry-kwon <jukdo67@gmail.com>
2021-10-06 20:28:23 +0900

    SearchResult.js 추가

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import Component from './src/js/components/Component.js';
 
 import Search from './src/js/components/Search/index.js';
+import SearchResult from './src/js/components/SearchResult.js';
 
 export default class App extends Component {
   constructor(target) {
-    super(target, 'div', { id: 'app' });
+    super(target, 'div', { id: 'App' });
     // this.bindEvents();
     // this.getData();
     // 하위 컴포넌트 정의
-    this.children = [new Search(this.$)];
+    this.children = [new Search(this.$), new SearchResult(this.$)];
   }
+
+  render = () => {
+    this.children.forEach(child => {
+      child.render && child.render();
+    });
+    document.querySelector('input').focus();
+  };
 }
